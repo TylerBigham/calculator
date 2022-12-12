0b3832e9a0169237349c9ab3a375554f65c1e242
Tyler Bigham <Tyler.Bigham.Full.Breakfast@gmail.com>
2022-12-12 09:06:13 -0500

    Change funtion calls, make operate function work

---
+++ script.js
@@ -0,0 +1,30 @@
+ 
+
+
+function add(addOne, addTwo){
+    return addOne + addTwo;
+}
+
+function subtract(subOne, subTwo){
+    return subOne - subTwo;
+} 
+
+function multiply(multOne, multTwo){
+    return multOne * multTwo;
+}
+
+function divide(divOne, divTwo){
+    return divOne / divTwo;
+}
+
+function operate(operand, numOne, numTwo) {
+    if (operand === "+" ) {
+        return add(numOne,numTwo);
+    }else if (operand === "-" ){
+        return subtract(numOne,numTwo);
+    }else if (operand === "*" ){
+        return multiply(numOne,numTwo);
+    }else if (operand === "/" ){
+        return divide(numOne,numTwo);
+    }
+}
